1debb378cef7bfd357822c8381c2b7b3e6b88549
gKushagra <55290773+gKushagra@users.noreply.github.com>
2021-10-27 22:54:29 -0500

    added dash views

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
 var authApiRouter = require('./routes/auth.api');
+var dashRouter = require('./routes/dash');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
-app.use('/api/vi/auth', authApiRouter);
+app.use('/api/v1/auth', authApiRouter);
+app.use('/dash', dashRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
